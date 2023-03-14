a74964918e0a60ee53be158672c4a64c4431665f
aquicor <aquinoacordero@gmail.com>
2023-03-14 23:03:17 +0100

    header avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
 
 import Logo from '../assets/img/logo.png';
-import {MdShoppingBasket} from 'react-icons/md';
+import Avatar from '../assets/img/avatar.png';
+import { MdShoppingBasket } from 'react-icons/md';
 
 export default function Header() {
     return (
-        <div className='fixed z-50 w-screen p-6 px-16'>
+        <header className='fixed z-50 w-screen p-6 px-16'>
             {/* Desktop */}
-            <div className='hidden md:flex w-full h-full'>
+            <div className='hidden md:flex w-full h-full items-center justify-between'>
                 <div className="flex items-center gap-2">
-                    <img src={Logo} className='w-10 object-cover' alt="logo"/>
+                    <img src={Logo} className='w-10 object-cover' alt="logo" />
                     <p className='text-headingColor text-xl font-bold'>City</p>
                 </div>
 
-                <ul className='flex items-center gap-8 ml-auto'>
-                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all cursor-pointer'>Home</li>
-                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all cursor-pointer'>Menu</li>
-                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all cursor-pointer'>About Us</li>
-                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all cursor-pointer'>Service</li>
-                </ul>
+                <div className='flex items-center gap-8'>
+                    <ul className='flex items-center gap-8'>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Service</li>
+                    </ul>
 
-                <div className='relative'>
-                    <MdShoppingBasket/>
+                    <div className='relative flex items-center justify-center'>
+                        <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
+                        <div className='absolute -top-0 -right-3 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                            <p className='text-xs text-white font-semibold'>2</p>
+                        </div>
+                    </div>
+
+                    <img src={Avatar} className="w-10 min-w-[40px] min-h-[40px]" alt="userprofile" />
                 </div>
             </div>
 
             {/* Mobile */}
-            <div className='flex md:hidden w-full h-full'>
-
-            </div>
-        </div>
+            <div className='flex md:hidden w-full h-full'></div>
+        </header>
     )
 }
---
+++ src/connection.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnaIbMBOeWjDJIfVaxQe0Qfa1l7INoODE",
+  authDomain: "food-delivery-99ad9.firebaseapp.com",
+  databaseURL: "https://food-delivery-99ad9-default-rtdb.firebaseio.com",
+  projectId: "food-delivery-99ad9",
+  storageBucket: "food-delivery-99ad9.appspot.com",
+  messagingSenderId: "380330790112",
+  appId: "1:380330790112:web:bff8959ddf7c07ed4a9485"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
