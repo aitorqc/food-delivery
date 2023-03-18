6f363e704a76229754a3e6fd4b73636130ced40c
aquicor <aquinoacordero@gmail.com>
2023-03-18 21:46:36 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
+
+import {Header, MainContainer, CreateContainer} from './components'
 
 export default function App() {
   return (
     <div className='w-screen h-auto flex flex-col'>
       <Header/>
+
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/*' element={<MainContainer/>}/>
+          <Route path='/createItem' element={<CreateContainer/>}/>
+        </Routes>
+      </main>
     </div>
   )
 }
