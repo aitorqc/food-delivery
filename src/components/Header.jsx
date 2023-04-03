ed42cf71d2461633f5bafb17c093aa2baa16a820
aquicor <aquinoacordero@gmail.com>
2023-04-03 17:53:11 +0200

    Show contextual user menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,18 @@ import { app } from '../firebase.config';
 
 import Logo from '../assets/img/logo.png';
 import Avatar from '../assets/img/avatar.png';
-import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 import { useStateValue } from '../context/StateProvider';
 import { actionType } from '../context/Reducer';
+import { useState } from 'react';
 
 export default function Header() {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
     const [{ user }, dispatch] = useStateValue();
+
+    const [isMenu, setIsMenu] = useState(false);
 
     const login = async () => {
         if (!user) {
@@ -61,10 +64,20 @@ export default function Header() {
                             onClick={login}
                         />
                     </div>
-                    <div>
-                        <p>New Item</p>
-                        <p>Logout</p>
-                    </div>
+                    {
+                        isMenu && (
+                            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+                                {
+                                    user && user.email == "aquinoacordero@gmail.com" && (
+                                        <Link to={"/createItem"}>
+                                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor'>New Item<MdAdd></MdAdd></p>
+                                        </Link>
+                                    )
+                                }
+                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor'>Logout<MdLogout></MdLogout></p>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
 
