aaef4a5f51d8a0248527cc0e61b35bd498123a2d
aquicor <aquinoacordero@gmail.com>
2023-04-15 23:39:24 +0200

    Logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,19 @@ export default function Header() {
                 user: providerData[0]
             });
             localStorage.setItem('user', JSON.stringify(providerData[0]));
+        }else{
+            setIsMenu(!isMenu);
         }
+    }
+
+    const logout = () =>{
+        setIsMenu(false);
+        localStorage.clear();
+
+        dispatch({
+            type: actionType.SET_USER,
+            user: null,
+        })
     }
 
     return (
@@ -66,15 +78,15 @@ export default function Header() {
                     </div>
                     {
                         isMenu && (
-                            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0'>
+                            <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-16 right-0'>
                                 {
-                                    user && user.email == "aquinoacordero@gmail.com" && (
+                                    user && user.email === "aquinoacordero@gmail.com" && (
                                         <Link to={"/createItem"}>
                                             <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor'>New Item<MdAdd></MdAdd></p>
                                         </Link>
                                     )
                                 }
-                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor'>Logout<MdLogout></MdLogout></p>
+                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor' onClick={logout}>Logout<MdLogout></MdLogout></p>
                             </div>
                         )
                     }
